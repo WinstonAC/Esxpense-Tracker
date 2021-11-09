1e4b2e57f606e42ef26a758a9636cbbd68a0a806
William <Billy.campbell17@gmail.com>
2021-11-09 17:34:57 -0500

    added CSS

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,5 +1,18 @@
+import './ExpenseItem.css'
+
+
+
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>August 29.21</div>
+      <div className="expense-item__description">
+        <h2 className>Car Insurance</h2>
+        <div className="expense-item__price">$444</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
